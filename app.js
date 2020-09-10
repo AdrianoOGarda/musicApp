eb1f3e0b50a0d8d125de5b17175ecc479207dbb9
eliot <eliot@pop-os.localdomain>
2020-09-10 16:32:42 -0500

    Corregido para Deploy en Heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const flash = require("connect-flash");
 const { setLocals } = require("./middlewares")
 
 mongoose
-    .connect(procces.env.DB, { useNewUrlParser: true })
+    .connect(process.env.DB, { useNewUrlParser: true })
     .then(x => {
         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     })
